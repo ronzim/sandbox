b8795f3412fe8a97243fefc862f5622b43047a21
Mattia Ronzoni <mattia.ronzoni@orobix.com>
2019-03-15 21:50:14 +0100

    add csv

--- bigDataIdeas/csv_analysis.js
+++ bigDataIdeas/csv_analysis.js
@@ -1,6 +1,7 @@
 const Papa = require('papaparse')
 const fs = require('fs-extra')
 const _ = require('underscore')
+var Plotly = require('plotly.js-dist')
 
 
 const file = fs.createReadStream('../material/heart.csv');
@@ -14,5 +15,43 @@ Papa.parse(file, {
       console.log(_.size(results), _.size(results.data))
       console.log(_.keys(results))
       console.log(results.meta.fields)
+
+      loadGraph(results);
     }
 });
+
+function loadGraph(results){
+  // console.log('here')
+  // console.log(result)
+  console.log(_.keys(results.data[0])[0])
+  var ages = _.pluck(results.data, _.keys(results.data[0])[0]);
+  var mhrs = _.pluck(results.data, "thalach");
+  var bps  = _.pluck(results.data, "trestbps");
+  // console.log(ages, mhr)
+
+  var trace = {
+    x: ages,
+    y: mhrs,
+    z: bps,
+    name: '',
+    mode: 'markers',
+    marker: {
+      size: 2,
+      color: _.pluck(results.data, "sex")
+    },
+    type: 'scatter3d',
+    // text:  _.pluck(days, 'travelTime')
+  };
+
+  var layout = {
+    xaxis: {},
+    yaxis: {
+      // label: 'travel time'
+    },
+    title:''
+  };
+
+  var data = [trace];
+
+  Plotly.newPlot('graph-container', data, layout, {responsive: true});
+}
