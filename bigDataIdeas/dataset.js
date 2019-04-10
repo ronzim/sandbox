c0ee4f1a1c1ccd232352d6f0c12e7547dfe830b3
Mattia Ronzoni <mattia.ronzoni@orobix.com>
2019-04-10 12:33:34 +0200

    solve logging error pbs

--- bigDataIdeas/dataset.js
+++ bigDataIdeas/dataset.js
@@ -3,12 +3,17 @@
 const _ = require('lodash');
 const uuid = require('uuid/v4');
 
-// TODO get datatype for each key
+var VERBOSE = false;
+
+// TODO list
+// - get datatype for each key & getter
+// - perform data coherence: eg. all entries has same keys (use defaults)
+// - check data depth (nested objects)
+// - getters / setters for axis vars
 
 class Dataset {
 
-  constructor(data){
-    console.log(aaa)
+  constructor(data) {
     this.rawData = data;
     // number of data entries
     this._size    = _.size(data);
@@ -21,11 +26,8 @@ class Dataset {
     this._treeIndex    = 0;
     this._filtersFuns  = [];
     this._filtersArgs  = [];
-    // data depth TODO ?
-    // data dimensions ?
-    // data size ?
+    // axis
     this._axisKeys = {};
-    // perform data coherence ? eg. all entries has same keys (use defaults)
     this.initNewBranch();
   }
 
@@ -33,23 +35,23 @@ class Dataset {
   // Getters ==
   // ==========
 
-  get keys(){
+  get keys() {
     return this._keys;
   }
 
-  get size(){
+  get size() {
     return this._size;
   }
 
-  get axisKeys(){
+  get axisKeys() {
     return this._axisKeys;
   }
 
-  get filtersTree(){
+  get filtersTree() {
     return this._filtersTree;
   }
 
-  get activeBranch(){
+  get activeBranch() {
     return {
       id   : this._activeBranch,
       funs : this._filtersFuns,
@@ -57,7 +59,7 @@ class Dataset {
     }
   }
 
-  get treeIndex(){
+  get treeIndex() {
     return this._treeIndex;
   }
 
@@ -65,8 +67,8 @@ class Dataset {
   // Setters ==
   // ==========
 
-  set activeBranch(id){
-    if (_.isString(id) && id.length == 36 && _.has(this._filtersTree, id)){
+  set activeBranch(id) {
+    if (_.isString(id) && id.length == 36 && _.has(this._filtersTree, id)) {
       this._activeBranch = id;
     }
     else{
@@ -74,9 +76,10 @@ class Dataset {
     }
   }
 
-  set treeIndex(index){
-    if (_.isFinite(index)){
+  set treeIndex(index) {
+    if (_.isFinite(index)) {
       this._treeIndex = index;
+      if (VERBOSE) console.log('index set to ', this._treeIndex);
     }
     else{
       console.error('index must be a number');
@@ -88,53 +91,52 @@ class Dataset {
   // ====================
 
   // setup a new branch, copying arrays to active index position
-  initNewBranch(){
-    console.log('new branch')
+  initNewBranch() {
     var currentId = this._activeBranchId;
     // for first initialization
-    if (currentId === null){
+    if (currentId === null) {
       var currentFuns = [];
       var currentArgs = [];
     }
     else{
-      var currentFuns = this._filtersTree[currentId].filtersFuns;
-      var currentArgs = this._filtersTree[currentId].filtersArgs;
+      var currentFuns = this._filtersTree[currentId].funs;
+      var currentArgs = this._filtersTree[currentId].args;
     }
     var newId = uuid();
     this._filtersTree[newId] = {
       parentId : currentId,
       id   : newId,
       funs : _.slice(currentFuns, 0, this._treeIndex),
-      args : _.slice(currentArgs, 0, this._treeIndex) // TODO adapt all functions to this new object (funs and args inside)
+      args : _.slice(currentArgs, 0, this._treeIndex)
     };
     // set as active branch
     this._activeBranchId = newId;
+    if (VERBOSE) console.log('create new branch', newId);
     return newId;
   }
 
-  addFilter(filter, value, insertPosition){
-    console.log(filter, value, insertPosition)
+  addFilter(filter, value, insertPosition) {
+    if (VERBOSE) console.log('add filter', filter, value, insertPosition)
     var currId = this._activeBranchId;
-    console.log('1', this._filtersTree, currId)
-    if (!insertPosition){
-      insertPosition = this._filtersTree[currId].filtersFuns.length;
+    if (VERBOSE) console.log('1', this._filtersTree, currId)
+    if (!insertPosition) {
+      insertPosition = this._filtersTree[currId].funs.length;
     }
-    this._filtersTree[currId].filtersFuns.splice(insertPosition, 0, filterMap[filter]);
-    this._filtersTree[currId].filtersArgs.splice(insertPosition, 0, value);
-    console.log(this._filtersTree[currId].filtersFuns);
+    this._filtersTree[currId].funs.splice(insertPosition, 0, filterMap[filter]);
+    this._filtersTree[currId].args.splice(insertPosition, 0, value);
   }
 
-  runFilter(){
+  runFilter() {
     var _this = this;
     var currId = this._activeBranchId;
-    var funcArray = Object.values(this._filtersTree[currId].filtersFuns);
-    console.log(funcArray)
-    var finalResult = funcArray.reduce( function(previousResult, fn, index){
-      var res = fn(previousResult, this._filtersTree[currId].filtersArgs[index]);
+    var funcArray = Object.values(this._filtersTree[currId].funs);
+    if (VERBOSE) console.log(funcArray)
+    var finalResult = funcArray.reduce( function(previousResult, fn, index) {
+      var res = fn(previousResult, this._filtersTree[currId].args[index]);
       return res;
     }, this.rawData);
 
-    console.log(finalResult.length);
+    if (VERBOSE) console.log(finalResult.length);
     return finalResult;
   }
 
@@ -163,38 +165,38 @@ const filterMap = {
   // TODO
 }
 
-function filterOnKeyValue(data, [key, value]){
-  console.log(data.length, '--', key, value);
-  var tap = _.filter(data, function(d){
+function filterOnKeyValue(data, [key, value]) {
+  if (VERBOSE) console.log(data.length, '--', key, value);
+  var tap = _.filter(data, function(d) {
     return d[key] === value;
   });
   return tap;
 }
 
-function filterOnKeyRange(data, [key, value1, value2]){
-  console.log(data.length, '--', key, value1, value2);
+function filterOnKeyRange(data, [key, value1, value2]) {
+  if (VERBOSE) console.log(data.length, '--', key, value1, value2);
   var low  = value1 < value2 ? value1 : value2;
   var high = value1 > value2 ? value1 : value2;
-  var tap = _.filter(data, function(d){
+  var tap = _.filter(data, function(d) {
     return d[key] >= low && d[key] <= high;
   });
   return tap;
 }
 
-function filterOnKeyTreshold(data, [key, threshold, sign]){
-  console.log(data.length, '--', key, threshold, sign);
-  var tap = _.filter(data, function(d){
+function filterOnKeyTreshold(data, [key, threshold, sign]) {
+  if (VERBOSE) console.log(data.length, '--', key, threshold, sign);
+  var tap = _.filter(data, function(d) {
     var res;
-    if (sign == '>'){
+    if (sign == '>') {
       res = parseFloat(d[key]) > threshold;
     }
-    else if (sign == '<'){
+    else if (sign == '<') {
       res = parseFloat(d[key]) < threshold;
     }
-    else if (sign == '>'){
+    else if (sign == '>') {
       res = parseFloat(d[key]) >= threshold;
     }
-    else if (sign == '<'){
+    else if (sign == '<') {
       res = parseFloat(d[key]) <= threshold;
     }
     return res;
@@ -202,42 +204,47 @@ function filterOnKeyTreshold(data, [key, threshold, sign]){
   return tap;
 }
 
-function fakeFilter(data, fakeArgs){
-  console.log(data.length, 'ff', fakeArgs)
+function fakeFilter(data, fakeArgs) {
+  if (VERBOSE) console.log(data.length, 'ff', fakeArgs)
   return data;
 }
 
-// for test
+// ===================================
+// TESTS =============================
+// ===================================
+
 
 const Papa = require('papaparse')
 const fs   = require('fs-extra')
 
-function init(rawData){
-  const file = fs.createReadStream('../material/heart.csv');
-  Papa.parse(file, {
-      header: true,
-      complete: function(results, file) {
-        console.log(_.size(results), _.size(results.data))
-        console.log(_.keys(results))
-        console.log(results.meta.fields)
+const file = fs.createReadStream('../material/heart.csv');
+// Papa.parse(file, {
+//     header: true,
+//     complete: function(results, file) {
+//       console.log(_.size(results.data))
+//       console.log(_.keys(results))
+//       console.log(results.meta.fields)
+//       setTimeout(initDs, 0, results.data)
+//     }
+// });
 
-        // init(results.data);
-        var ds = new Dataset(results.data);
-        ds.addFilter('keyValue', ['sex', 1]);
-        ds.addFilter('keyValue', ['sex', 1]);
-        ds.addFilter('keyValue', ['sex', 1]);
-        ds.addFilter('keyTreshold', [' age', 50], 1);
-        var b0 = ds.activeBranch.id;
-        ds.treeIndex = 1;
-        var b1 = ds.initNewBranch();
-        // console.log(b0, b1);
-        // console.log(ds.activeBranch)
-        //
-        // console.log(ds.treeIndex)
-      }
-  });
+function initDs(rawData) {
+  var ds = new Dataset(rawData);
+  console.log('1 ------------------------------')
+  ds.addFilter('keyValue', ['sex', 1]);
+  ds.addFilter('keyValue', ['sex', 1]);
+  ds.addFilter('keyValue', ['sex', 1]);
+  ds.addFilter('keyTreshold', [' age', 50], 1);
+  console.log('2 ------------------------------')
+  var b0 = ds.activeBranch.id;
+  ds.treeIndex = 1;
+  console.log('3 ------------------------------')
+  var b1 = ds.initNewBranch();
+  console.log('4 ------------------------------')
+  console.log(b0, b1);
+  console.log(ds.activeBranch)
+  console.log(ds.treeIndex)
 }
-init()
 
 exports.Dataset = Dataset;
-exports.init = init;
+exports.setVerbose = function(toggle) {VERBOSE = toggle};
