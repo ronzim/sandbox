21551af885d77f2add194bbd9cecc5393db8cd12
ronzim <mat.ronzoni@gmail.com>
2019-05-27 23:12:50 +0200

    make BBbot useful

--- BBbot/BBbot.py
+++ BBbot/BBbot.py
@@ -4,22 +4,20 @@
 # set hook API
 # https://api.telegram.org/bot<TOKEN>/setWebhook?url=https://hook.io/<hook-user>/<hook-name>
 
-from telegram.ext import Updater
-updater = Updater(token='763533825:AAEsXkIYKmJ7ts61ZE2QEFc0w5-Vmwwpcbs')
-
-dispatcher = updater.dispatcher
-
+import json
 import logging
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+updater = Updater(token='763533825:AAEsXkIYKmJ7ts61ZE2QEFc0w5-Vmwwpcbs')
+dispatcher = updater.dispatcher
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-import json
 def append(obj):
     previus_data = loadFile()
     previus_data.append(obj)
@@ -31,39 +29,80 @@ def loadFile():
         data = json.load(f)
         return data
 
+global current_value
 def parseMsg(msg):
-    value = msg.text.split()
-    if (len(value)<3):
-        return
-    print value[0]
-    print value[1]
-    return {'date':msg.date.strftime("%Y-%m-%d %H:%M:%S"), 'key':value[1], 'value':value[2]}
+    # value = msg.text.split()
+    # if (len(value)>1):
+    #     return False
+    return {'date':msg.date.strftime("%Y-%m-%d %H:%M:%S"), 'key': msg.text, 'value':current_value}
 
-def something(bot, update):
-    print update.message.date
-    print update.message.text
-    entry = parseMsg(update.message)
+def read_storage(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='--- init storage ---')
+    storage = loadFile()
+    for l in range(0, len(storage)):
+        entry = storage[l]
+        if not entry: continue
+        line = entry['date'] + ' || ' + entry['key'] + ' || ' + str(entry['value'])
+        bot.send_message(chat_id=update.message.chat_id, text=line)
+    bot.send_message(chat_id=update.message.chat_id, text='--- end storage ---')
+
+categories = [
+    'benzina',
+    'auto',
+    'pranzo',
+    'cena/ape',
+    'acquisti',
+    'altro'
+]
+
+def registerEntry(bot, msg):
+    # if (not entry):
+    #     bot.send_message(chat_id=update.message.chat_id, text='not enough arguments')
+    #     return
+    entry = parseMsg(msg)
+    entry['value'] = current_value
     append(entry)
-    bot.send_message(chat_id=update.message.chat_id, text='---')
+    print entry
+    ans = 'registered >> ' + entry['date'] + ' || ' + str(entry['key']) + ' || ' + str(entry['value'])
+    bot.send_message(chat_id=msg.chat_id, text=ans)
+    global current_value
+    current_value = None
 
-from telegram.ext import CommandHandler
-something_handler = CommandHandler('something', something)
-dispatcher.add_handler(something_handler)
-
-
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-
-from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
+def handle_msg(bot, update):
+    if update.message.text in categories:
+        registerEntry(bot, update.message)
+    else:
+        # TODO check if numeric value
+        global current_value
+        current_value = update.message.text
+    # reply_keyboard = [[telegram.KeyboardButton(text='aaa'), telegram.KeyboardButton(text='/bbb')]]
+    # close_btn = [telegram.KeyboardButton(telegram.KeyboardEnum.CANCEL.clean())]
+        reply_keyboard = []
+        for c in range(0, len(categories)/3):
+            keyboard_line = [
+                telegram.KeyboardButton(text=categories[c]),
+                telegram.KeyboardButton(text=categories[c+1]),
+                telegram.KeyboardButton(text=categories[c+2])
+            ]
+            reply_keyboard.append(keyboard_line)
+        close_btn = [telegram.KeyboardButton('close')]
+        bot.sendMessage(chat_id=update.message.chat_id, text='scelta categoria', parse_mode='HTML',
+                        reply_markup=telegram.ReplyKeyboardMarkup(reply_keyboard, footer_buttons=close_btn, one_time_keyboard=True))
 
 def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
 
+# handlers binding
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+getstorage_handler = CommandHandler('getstorage', read_storage)
+dispatcher.add_handler(getstorage_handler)
+msg_handler = MessageHandler(Filters.text, handle_msg)
+dispatcher.add_handler(msg_handler)
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
 
+# start bot
 updater.start_polling()
 # updater.stop()
 updater.idle()
